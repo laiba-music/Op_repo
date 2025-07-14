7c53c7aed6d72a194a48cac66c83e3df4d478c80
iamnobita08 <nobitamusicrobot@gmail.com>
2025-07-14 13:27:31 +0530

    Add files via upload

---
+++ NOBITA/__init__.py
@@ -0,0 +1,28 @@
+from NOBITA.core.bot import NOBITA
+from NOBITA.core.dir import dirr
+from NOBITA.core.git import git
+from NOBITA.core.userbot import Userbot
+from NOBITA.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = NOBITA()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
