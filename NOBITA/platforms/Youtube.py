4e6da3fa44a06715c93a8c9f75b60953b88d090a
iamnobita08 <nobitamusicrobot@gmail.com>
2025-07-20 16:02:53 +0530

    Update Youtube.py

--- NOBITA/platforms/Youtube.py
+++ NOBITA/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from Clonify.utils.database import is_on_off
-from Clonify.utils.formatters import time_to_seconds
+from NOBITA.utils.database import is_on_off
+from NOBITA.utils.formatters import time_to_seconds
 
 
 def cookies():
